661b8b3eeb64917090e27268165b53f1efde1ebe
dongjingxi <dongjingqian@uniontech.com>
2021-02-22 15:41:16 +0800

    feat: init

---
+++ build/CMakeFiles/CalculateSqrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculateSqrt.dir/calculateSqrt.cpp.o"
+  "CalculateSqrt.pdb"
+  "CalculateSqrt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculateSqrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CalculateSqrtConfig.h
@@ -0,0 +1,4 @@
+// the configured options and settings for CalculateSqrt，@@引用的变量可以通过CMakeLists.txt来设置 
+#define CalculateSqrt_VERSION_MAJOR 1
+#define CalculateSqrt_VERSION_MINOR 0
+// #define USE_MYMATH 
